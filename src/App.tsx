589145d8aba4f2a0eaf34ea8814ea420e876ee03
ManI <letz0703@gmail.com>
2022-10-23 08:18:22 +0900

    setup & design useMultisteopForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
 function App() {
-  return <h1>Hi</h1>
+  return (
+    <div
+      style={{
+        //
+        position: "relative",
+        background: "white",
+        border: "1px solid black",
+        padding: "2rem",
+        margin: "1rem",
+        borderRadius: ".5rem",
+        fontFamily: "Arial"
+      }}
+    >
+      <form></form>
+    </div>
+  )
 }
 
 export default App
---
+++ src/useMultistepFrom.ts
@@ -0,0 +1,24 @@
+import {ReactElement, useState, useEffect} from "react"
+
+export const useMultistepFrom = (steps: ReactElement[]) => {
+  const [currentStepIndex, setCurrentStepIndex] = useState(0)
+
+  function next() {
+    setCurrentStepIndex((i) => {
+      if (i >= steps.length - 1) return i
+      return i + 1
+    })
+  }
+  function back() {
+    setCurrentStepIndex((i) => {
+      if (i <= 0) return i
+      return i - 1
+    })
+  }
+
+  const goto = (index: number) => {
+    setCurrentStepIndex(index)
+  }
+
+  return {currentStepIndex, step: steps[currentStepIndex], goto, next, back}
+}
